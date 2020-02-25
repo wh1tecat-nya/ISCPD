0c13e44646c277bbb444009faaf1b528858c9937
wh1tecat <wh1tecat.sec@gmail.com>
2020-02-25 13:35:17 +0900

    表示確認用

--- modules/ISCPD-Web/server.js
+++ modules/ISCPD-Web/server.js
@@ -1,10 +1,11 @@
+const path = require("path");
 const express = require("express");
 const app = express();
 
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-	res.status(200).sendFile("./public/index.html");
+	res.status(200).sendFile(path.resolve(__dirname, "./public/index_old2.html"));
 });
 
 app.listen(9000);
