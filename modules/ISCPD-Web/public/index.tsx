b3b5f135905650d12a7e2c75c94f614acfd5d324
wh1tecat <wh1tecat.sec@gmail.com>
2020-02-25 13:34:51 +0900

    Publicなファイルの場所変えた

--- modules/ISCPD-Web/public/index.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react";
-import { OfficialAbsence } from "ISCPD-Contents";
-import { render } from "react-dom";
-
-render(<OfficialAbsence />, document.getElementById("root"));
